62e965e6b10cef48fcae4c5887ebb2f4a8df271a
github username <github email address>
2018-10-01 15:01:37 -0400

    cleaned some of the comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -26,19 +26,4 @@ export const getBoard = (board_id) => {
     type: 'GET_BOARD',
     payload: board_id
   }
-  // return (dispatch) => {
-  //   console.log(dispatch)
-  //   fetch(`http://localhost:4000/api/v1/boards/${board_id}`)
-  //   .then(r=>r.json())
-  //   .then(r=> {
-  //     console.log(r)
-  //     dispatch({
-  //       type: 'GET_BOARD',
-  //       payload: {
-  //         "board":r.board,
-  //         "posts":r.posts
-  //       }
-  //     })
-  //   })
-  // }
 }
